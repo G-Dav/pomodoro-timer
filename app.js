f51540d62ad2353362717c456276a502372f4fd7
GD <geny.d.mtz@hotmail.com>
2021-03-02 17:12:16 -0600

    Añadí el código para cambiar los tipos de intevalos (sin que exista otra sesión en transcurso)

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-// Objeto tiempo que define la duración de una sesión
-let tiempoAux = {
-    min: 0,
-    seg: 5
+var conf = {
+    pomodoro: {min: 25},
+    dCorto: {min: 5},
+    dLargo: {min: 15},
+    sesiones: 0
 }
+
+// Objeto tiempo que define el tiempo restante de una sesión
+let tiempoAux = {
+    min: 25,
+    seg: 0
+}
+
 const tiempoPantalla = document.getElementById('tiempo')
 const btnInicio = document.getElementById('start')
 const btnReset = document.getElementById('reset')
+const btnSesion = document.getElementById('pomodoro')
+const btndCorto = document.getElementById('shortBreak')
+const btndLargo = document.getElementById('longBreak')
 btnInicio.dataset.id = 1
 let intervalo
+let tipoSesion = 1; // 1: pomodoro, 2: descanso corto, 3: descanso largo
 
 // Iniciar la cuenta regresiva al presionar el botón Start
 // Cambiar el estado del botón entre Start y Stop en cada click
 btnInicio.addEventListener('click', () =>{
     if(btnInicio.dataset.id == 1){
-        console.log("start")
         btnInicio.dataset.id = 2
         btnInicio.innerText = "Stop"
         intervalo = setInterval("restarTiempo()", 1000);
+        console.log("start"+intervalo)
     }else{
-        console.log("stop")
         btnInicio.dataset.id = 1
         btnInicio.innerText = "Start"
         clearInterval(intervalo)
+        console.log("stop"+intervalo)
     }
 })
 
+// Función que realiza la cuenta regresiva
 const restarTiempo = () => {
     tiempoPantalla.innerHTML = formatoTiempo(tiempoAux.min, tiempoAux.seg)
     if(tiempoAux.seg == 0){
@@ -32,9 +45,24 @@ const restarTiempo = () => {
         if(tiempoAux.min < 0){
             // detener la cuenta regresiva 
             clearInterval(intervalo)
-            // Reiniciar el tiempo a sus valores originales
-            tiempoAux.min = 0
-            tiempoAux.seg = 5
+            intervalo = false
+            // Asignar el tiempo de la sesión siguiente según la sesión actual
+            switch(tipoSesion){
+                case 1:
+                    tiempoAux.min = conf.dCorto.min
+                    tipoSesion = 2
+                    break;
+                case 2:
+                    tiempoAux.min = conf.pomodoro.min
+                    tipoSesion = 1
+                    break;
+                case 3:
+                    tiempoAux.min = conf.pomodoro.min
+                    tipoSesion = 1
+                    break;
+            }
+            tiempoAux.seg = 0
+
             // Restablecer los valores del boton start
             btnInicio.dataset.id = 1
             btnInicio.innerText = "Start"
@@ -48,15 +76,59 @@ const restarTiempo = () => {
 
 // Para mostrar el tiempo en el formato mm:ss
 const formatoTiempo = (minutos, segundos) => {
-    var m = minutos<9?"0"+minutos:minutos
-    var s = segundos<9?"0"+segundos:segundos
+    var m = minutos<10?"0"+minutos:minutos
+    var s = segundos<10?"0"+segundos:segundos
     return m + ":" + s
 }
 
 // Reiniciar el contador con el botón reset
-btnReset.addEventListener('click', () =>{
+btnReset.addEventListener('click', () => {
+    btnInicio.dataset.id = 1
+    btnInicio.innerText = "Start"
     clearInterval(intervalo)
-    tiempoAux.min = 0
-    tiempoAux.seg = 5
+    intervalo = false
+    switch(tipoSesion){
+        case 1:
+            tiempoAux.min = conf.pomodoro.min
+            break;
+        case 2:
+            tiempoAux.min = conf.dCorto.min
+            break;
+        case 3:
+            tiempoAux.min = conf.dLargo.min
+            break;
+    }
+    tiempoAux.seg = 0
+    tiempoPantalla.innerHTML = formatoTiempo(tiempoAux.min, tiempoAux.seg)
+})
+
+// Cambiar a descanso corto
+btndCorto.addEventListener('click', ()=> {
+    if(!intervalo){
+        tipoSesion = 2
+        tiempoAux.min = conf.dCorto.min
+        tiempoAux.seg = 0
+        tiempoPantalla.innerHTML = formatoTiempo(tiempoAux.min, tiempoAux.seg)
+    }
+    else{
+        console.log("hay una sesion en curso")
+        window.alert(":v")
+        window.confirm("x2")
+    }
+})
+
+// Cambiar a descanso largo
+btndLargo.addEventListener('click', () => {
+    tipoSesion = 3
+    tiempoAux.min = conf.dLargo.min
+    tiempoAux.seg = 0
+    tiempoPantalla.innerHTML = formatoTiempo(tiempoAux.min, tiempoAux.seg)
+})
+
+// Cambiar a sesión pomodoro
+btnSesion.addEventListener('click', () => {
+    tipoSesion = 1
+    tiempoAux.min = conf.pomodoro.min
+    tiempoAux.seg = 0
     tiempoPantalla.innerHTML = formatoTiempo(tiempoAux.min, tiempoAux.seg)
 })
